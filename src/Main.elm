6df1869cf8670972aeb353e60db156dd4e209599
Roman Kotelnikov <roman.kotelnikov@outlook.com>
2020-03-21 20:44:32 +0800

    Working touch control

--- src/Main.elm
+++ src/Main.elm
@@ -8,16 +8,15 @@ import Math.Vector2 as Vec2 exposing (vec2, Vec2)
 import Math.Vector4 exposing (vec4, Vec4)
 import WebGL exposing (Mesh, Shader)
 
-type alias Model = { width: Int, height: Int, pos: Vec2 }
+type alias Model = { width: Int, height: Int, pos: Vec2, from: Vec2 }
 init: Model
-init = { width = 160, height = 240, pos = (vec2 0.0 0.0)}
+init = { width = 160, height = 240, pos = (vec2 0.0 0.0), from = (vec2 0.0 0.0)}
 
 type TouchEvent
     = None
-    | Start Touch.Event
-    | Move Touch.Event
-    | End Touch.Event
-    | Cancel Touch.Event
+    | Start (Float, Float)
+    | Move (Float, Float)
+    | End (Float, Float)
 
 type Msg = TouchEvent
 
@@ -43,24 +42,47 @@ makeEntity pos res = WebGL.entity
                 (rect pos 16.0 16.0)
                 { u_res = res } 
 
+touchCoordinates : Touch.Event -> ( Float, Float )
+touchCoordinates touchEvent =
+    List.head touchEvent.changedTouches
+        |> Maybe.map .clientPos
+        |> Maybe.withDefault ( 0, 0 )
+
 view: Model -> Html TouchEvent
 view model =
         WebGL.toHtml 
         [ 
-          Touch.onStart Start,
-          Touch.onMove Move,
-          Touch.onEnd End,
-          Touch.onCancel Cancel,
+          Touch.onStart (Start << touchCoordinates),
+          Touch.onMove (Move << touchCoordinates),
+          Touch.onEnd (End << touchCoordinates),
           width model.width,
           height model.height,
           style "backgroundColor" "#000000",
           style "display" "block" ]
         [ makeEntity model.pos (vec2 (toFloat model.width) (toFloat model.height) ) ]
 
+moveTo: Model -> Vec2 -> Vec2
+moveTo model to =
+        Vec2.sub to model.from
+        |> Vec2.add model.pos
+        |> \p -> vec2
+                    ( Basics.clamp 0.0 (toFloat model.width - 16) (Vec2.getX p) )
+                    ( Basics.clamp 0.0 (toFloat model.height - 16) (Vec2.getY p) )
+
+
 update: TouchEvent -> Model -> Model
 update event model =
     case event of
-        End ev -> model
+        Start (x, y) -> {
+            width = model.width,
+            height = model.height,
+            pos = model.pos,
+            from = vec2 x y }
+        Move (x, y) -> {
+            width = model.width,
+            height = model.height,
+            pos = moveTo model (vec2 x y) ,
+            from = vec2 x y }
         _ -> model
 
 vertexShader : Shader Vertex { u_res: Vec2 } {}
