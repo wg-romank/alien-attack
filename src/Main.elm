dac384c49abd630852124d61eebfe8c208c4a8df
Roman Kotelnikov <roman.kotelnikov@outlook.com>
2020-03-21 16:22:54 +0800

    Triangles

--- src/Main.elm
+++ src/Main.elm
@@ -2,9 +2,10 @@ module Main exposing (..)
 
 import Browser
 import Html exposing (Html)
-import Html.Attributes exposing (width, height)
+import Html.Attributes exposing (width, height, style)
 import Html.Events.Extra.Touch as Touch
 import Math.Vector2 exposing (vec2, Vec2)
+import Math.Vector3 exposing (vec3, Vec3)
 import WebGL exposing (Mesh, Shader)
 
 type alias Model = Int
@@ -24,43 +25,51 @@ type Msg = TouchEvent
 type alias Vertex =
     { position : Vec2 }
 
-x : Int -> Mesh Vertex
-x pos = WebGL.points [ List.map (\p -> Vertex (vec2 (toFloat p) (toFloat p) ) ) (List.range 1 pos) ]
+-- x : Int -> Mesh Vertex
+-- x pos = WebGL.points [ List.map (\p -> Vertex (vec2 (toFloat p) (toFloat p) ) ) (List.range 1 pos) ]
+
+
+mesh : Mesh Vertex
+mesh =
+    WebGL.triangles
+        [ ( Vertex (vec2 0 0)
+          , Vertex (vec2 1 1)
+          , Vertex (vec2 1 -1)
+          ) ]
 
 makeEntity: Int -> WebGL.Entity
 makeEntity pos = WebGL.entity
                 vertexShader
                 fragmentShader
-                (x pos) { 
-                    resolution = vec2 640 480
-                } 
+                mesh
+                -- (x pos) { 
+                { } 
+                -- { u_resolution = vec2 640 480 } 
 
 view: Model -> Html msg
 view pos = WebGL.toHtml 
-        [ width 640, height 480 ]
+        [ width 640, height 480, style "backgroundColor" "#9ea7b8", style "display" "block" ]
         [ makeEntity pos ]
 
-vertexShader : Shader { position: Vec2 } { resolution: Vec2 } { }
+vertexShader : Shader { position: Vec2 } { } { }
 vertexShader = 
     [glsl|
         attribute vec2 position;
-        uniform vec2 resolution;
 
         void main() {
-            vec2 zeroOne = position / resolution;
-            vec2 zeroTwo = position * 2.0;
-            vec2 clipSpace = zeroTwo - 1.0;
-            gl_Position = vec4(clipSpace * vec(1, -1), 0, 1);
+            gl_Position = vec4(position, 0, 1);
         }
     |]
+            -- vec2 zeroOne = position / resolution;
+            -- vec2 zeroTwo = position * 2.0;
+            -- vec2 clipSpace = zeroTwo - 1.0;
+            -- gl_Position = vec4(clipSpace * vec(1, -1), 0, 1);
 
-fragmentShader : Shader {} { resolution: Vec2 } { }
+fragmentShader : Shader {} { } { }
 fragmentShader =
     [glsl|
-        uniform vec2 resolution;
-
         void main() {
-            gl_FragColor = vec4(120, 120, 120, 120);
+            gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
         }
     |]
 
